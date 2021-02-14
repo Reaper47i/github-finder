4723f381cdf5950c307de01fe49b120ef06ac5e8
Reaper47i <vineetbhengra96@gmail.com>
2021-02-14 13:14:50 +0530

    first commit

---
+++ dino/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/Users/Users";
+import User from "./components/Users/User";
+import Search from "./components/Users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+import "./App.css";
+
+import GithubState from "./context/github/GithubState";
+
+const App = () => {
+  const [alert, setAlert] = useState(null);
+
+  // async componentDidMount(){
+  //   this.setState({loading: true});
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?
+  //     client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+  //     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //     );
+
+  //   this.setState({users: res.data, loading: false});
+  // }
+
+  //clear users from state
+
+  //SetAlert
+  const showAlert = (msg, type) => {
+    setAlert({ msg, type });
+    setTimeout(() => setAlert(null), 3000);
+  };
+
+  return (
+    <GithubState>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search setAlert={showAlert} />
+                    <Users />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/user/:login" component={User} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </GithubState>
+  );
+};
+
+export default App;
